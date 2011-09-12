c387d2030fc262f563cb46b55776641cea44b2d9
Ilya Petrov <ilya.muromec@gmail.com>
2011-09-13 02:04:10 +0300

    recenter

--- data/js/map.js
+++ data/js/map.js
@@ -24,6 +24,8 @@ var Hex = function(paper, x, y, w, h) {
     };
 
     hex.node.onclick = function() {
+       alert(hex.gen);
+
        hex.use(true);
        hex.attr("fill", "blue");
     }
@@ -41,6 +43,10 @@ var Map = function(rows, cols) {
 
     this.rows = rows;
     this.cols = cols;
+    this.center = [Math.round(cols/4)*2, Math.round(rows/4)*2];
+
+    var shift_x = Math.round(cols/3);
+    var shift_y = Math.round(rows/3);
 
     for(col=0;col<cols;col++) {
         this.cells[col] = [];
@@ -73,9 +79,10 @@ var Map = function(rows, cols) {
         var col = this.cells[user.x + o_x];
 
         next = col[user.y + o_y];
+        console.log(F("next hex get {0}", [next.gen]));
 
         console.log(F("next {0}x{1} @{2} {3}x{4}", [
-                    o_x, o_y, next, col.length, user.y+o_y]));
+                    o_x, o_y, next, user.x + o_x, user.y+o_y]));
 
         if(!next || next.used) {
             console.log("fuckup");
@@ -90,6 +97,151 @@ var Map = function(rows, cols) {
 
         user.x = next.col;
         user.y = next.row;
+
+        this.recenter(user.x, user.y);
+
+    };
+
+    this.add_hexes = function(id) {
+        var cols = this.center[0] + Math.round(this.cols/2),
+            rows = this.center[1] + Math.round(this.rows/2);
+
+        var start_row = rows - this.rows,
+            start_col = cols - this.cols;
+
+        console.log("start_col "+start_col);
+
+        for(var col=start_col; col<cols; col+=2) {
+
+            if((this.cells[col] === undefined)) {
+                    this.cells[col] = [];
+                    console.log("create col " + col);
+            }
+
+            for(row=start_row; row<rows; row+=2) {
+                var y = ((row-start_row) * HEX_H) + 40, _y = 0;
+                var _row = 0;
+
+
+                if((col % 4)!=0) {
+                    _y = HEX_H;
+                    _row = row + 1;
+
+                } else {
+                    _y = 0;
+                    _row = row;
+                }
+
+
+                var hex = this.cells[col][_row];
+                if(hex !== undefined) {
+                    hex.show();
+                    continue;
+                }
+
+                console.log(F("create {0}x{1}", [col, _row]));
+
+                var x = (col-start_col) * HEX_W;
+
+                hex = this.paper.hex(x, y + _y);
+                hex.text = this.paper.text(x + HEX_H, y + _y , Raphael.format("{0}x{1}", [col, _row]));
+                hex.col = col;
+                hex.row = _row;
+
+                this.cells[col][_row] = hex;
+
+                hex.free();
+                hex.gen = id;
+            }
+        }
+
+    };
+
+    this.recenter = function(x, y) {
+        var cx = this.center[0],
+            cy = this.center[1];
+
+        var _off_x = cx - x,
+            _off_y = cy - y;
+
+        var off_x = _off_x,
+            off_y = _off_y;
+
+        if(off_x < 0)
+            off_x = - off_x;
+
+        if(off_y < 0)
+            off_y = - off_y;
+
+        if((off_x < shift_x) && (off_y < shift_y))
+            return;
+
+        var move_x = _off_x * HEX_W,
+            move_y = _off_y * HEX_H;
+
+        var col_lim = this.cells.length;
+
+        var move = function(col_n) {
+            var col_off = col_n - x;
+
+            if(col_off < 0)
+                col_off = -col_off;
+
+            var drop_col = (col_off > shift_x);
+
+            var move_col = function() {
+
+                clearInterval(move_col.stop);
+
+                var col = this.cells[col_n];
+
+
+                for(var row_n=0; row_n < col.length; row_n++) {
+
+                    var row_off = row_n - y;
+                    if(row_off <0)
+                        row_off = - row_off;
+
+                    var drop_row = (row_off > shift_y);
+
+                    var hex = col[row_n];
+
+                    if(!hex)
+                        continue;
+
+                    if(drop_col || drop_row) {
+
+                        hex.text.hide()
+                        hex.hide()
+
+                        //col.splice(row_n, 1);
+                    } else {
+                        hex.translate(move_x, move_y);
+                        hex.text.translate(move_x, move_y);
+
+                    }
+
+                }
+
+                /*
+                if(drop_col) {
+                    this.cells.splice(col_n, 1);
+                } */
+
+                if(this.cells[col_n+1]) {
+                    move(col_n+1);
+                } else {
+                    this.center = [Math.round(x/2)*2, Math.round(y/2)*2];
+                    this.add_hexes(2);
+                }
+
+
+            }
+
+            move_col.stop = window.setInterval(move_col, 0);
+        }
+
+        move(0);
 
 
     };
@@ -114,40 +266,16 @@ var draw_map = function(el) {
         return new Hex(paper, x, y, HEX_W, HEX_H);
     }
 
-    var row, rows = 25,
-        col, cols = 35;
+    var row, rows = 24,
+        col, cols = 32;
 
     var map = Map(rows, cols);
+    map.paper = paper;
+    map.add_hexes(1);
+    alert(map.center);
 
-    for(row=0; row<rows; row+=2) {
-        var y = (row * HEX_H) + 40, _y = 0;
-        var _row = 0;
 
-        for(col=0; col<cols; col+=2) {
-
-            if((col % 4)==0) {
-                _y = HEX_H;
-                _row = row + 1;
-
-            } else {
-                _y = 0;
-                _row = row;
-            }
-
-            var x = col * HEX_W;
-
-            var hex = paper.hex(x, y + _y);
-            hex.text = paper.text(x + HEX_H, y + _y , Raphael.format("{0}x{1}", [col, _row]));
-            hex.col = col;
-            hex.row = _row;
-
-            map.cells[col][_row] = hex;
-
-            hex.free();
-        }
-    }
-
-    var user = map.add_user(6, 6);
+    var user = map.add_user(8, 8);
 
     key('l', function() {map.move(user, 2, 1)});
     key('o', function() {map.move(user, 2, -1)});
