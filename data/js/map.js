52386d71d1462a16c74c706bba6ab176bfa83a79
Ilya Petrov <ilya.muromec@gmail.com>
2011-09-18 20:46:12 +0300

    add fire animation

--- data/js/map.js
+++ data/js/map.js
@@ -430,6 +430,47 @@ var User = function(uid, x, y, img) {
 
     this.img = img;
 
+    this.fire = function() {
+        console.log('fire');
+        this.firesprite = 0;
+        var user = this;
+
+        var animate_fire = function(_e) {
+            console.log("animate fire "+user.firesprite);
+
+            user.img.attr("src",
+                    F(
+                        "/img/char/{0}_{1}_fire{2}.png",
+                        [
+                            user.img._img,
+                            user.img._sprite,
+                            user.firesprite
+                        ]
+            ));
+
+            user.firesprite++;
+
+            if(user.firesprite > 3) {
+                clearInterval(user._fire);
+                user._fire = null;
+                user.img.attr("src",
+                    F(
+                        "/img/char/{0}_{1}.png", [
+                            user.img._img,
+                            user.img._sprite
+                        ]
+                    )
+                );
+            }
+
+
+        }
+
+        if(!this._fire)
+            this._fire = window.setInterval(animate_fire, 300);
+
+    };
+
     return this;
 };
 
@@ -462,6 +503,9 @@ var draw_map = function(el) {
 
     key('j', function() {map.move(user, 0, 2)});
     key('k', function() {map.move(user, 0, -2)});
+
+    key('f', function() {user.fire()});
+
 
     key('i', function() {
         user = map.add_user(8, 8, 'faery');
