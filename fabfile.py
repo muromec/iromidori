42c7df75babe509ab19f6b1221c588f75b549771
Ilya Petrov <ilya.muromec@gmail.com>
2012-05-16 02:42:28 +0700

    working on deploy

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,10 @@
 from fabric.operations import put
+from fabric.api import cd, run
+
 
 def deploy():
-    put("data/js/*js", "serve/data/js/")
+    put("data/midori.min.js", "serve/data/midori.min.js")
     put("data/img/char/*sprite.png", "serve/data/img/char/")
+    with cd('serve'):
+        run('git reset --hard')
+        run('git pull --rebase')
